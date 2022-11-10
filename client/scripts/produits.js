8aeded375c785bc125e32b6f0585a156743a6a1a
gtchi <giovani.tchibozo@yahoo.fr>
2022-11-10 09:57:57 -0500

    modif

--- client/scripts/produits.js
+++ client/scripts/produits.js
@@ -2,13 +2,6 @@ $(function () {
     console.log("ift215");
 });
 
-$.ajax({
-    url: "/produits",
-    success: function( result ) {
-    console.log(result);
-    }
-   });
-
 function chargerproduit() {
     $.ajax({
         url: "/produits",
